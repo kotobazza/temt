09a4942e4f825c5e60d2c2f92d1ab57009817acd
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-16 01:22:52 +0300

    diskinfo part moved

---
+++ src/temt/DiskInfo.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+namespace temt {
+
+namespace DiskInfo {
+struct StoragePointInfo {
+    std::string deviceName{};
+    std::string mountPoint{};
+    std::string fileSystem{};
+    unsigned long int totalMemory{0};
+    unsigned long int freeMemory{0};
+};
+
+std::vector<StoragePointInfo> getMountedPoints();
+
+}  // namespace DiskInfo
+}  // namespace temt
