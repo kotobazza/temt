08f6adbe2e4b4cd7135a2e7ba0ff2252ecc4c9a2
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-23 01:06:02 +0300

    Included all headers into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "LogInitializer.hpp"
+#include "ArchiveManip.hpp"
+#include "FileManip.hpp"
+#include "CharProcessing.hpp"
+#include "DiskInfo.hpp"
 
 int main() {
     temt::Log::createFileLogger("logs/logs.txt");
