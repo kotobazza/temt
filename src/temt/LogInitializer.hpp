bd58ec77f903851ed42c7fe292a7c1b309455636
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-22 20:32:58 +0300

    updated name for loginitializer function

--- src/temt/LogInitializer.hpp
+++ src/temt/LogInitializer.hpp
@@ -6,7 +6,7 @@
 
 namespace temt {
 namespace Log {
-void createMainLogfile(std::string_view log_path);
+void createFileLogger(std::string_view log_path);
 
 }  // namespace Log
 }  // namespace temt
