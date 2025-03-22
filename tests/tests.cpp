004133e488a58cf4414023424d9756c5fc55aa8b
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-03-22 19:27:38 +0300

    test: created test for filemanip:clearNonRelativePath

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
+#include "FileManip.hpp"
 
 TEST(HelloWorld, GTestUsage) {
     int i = 0;
     EXPECT_EQ(i, 0);
+}
+
+
+TEST(FileManipulation, ClearNonRelativePathTest) {
+    std::string path{"../../hello"};
+    EXPECT_EQ(temt::FileManip::clearNonRelativePath(path), "hello");
 }
