3574a5048ce7fe7d294e16d6dc90a9921964c42f
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-03-27 11:14:34 +0300

    fix: broken logging feature

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,21 @@
 #include "CharProcessing.hpp"
 #include "DiskInfo.hpp"
 
+// int main() {
+//     temt::Log::createFileLogger("logs/logs.txt");
+//     return 0;
+// }
+
+
+// main.cpp
+#include "main_app.hpp"
+
 int main() {
     temt::Log::createFileLogger("logs/logs.txt");
+    auto log_file = spdlog::get("file_logger");
+    log_file->info("main: created logger");
+    auto app = MainApp();
+    log_file->info("main: created mainapp");
+    app.Run();
     return 0;
 }
