85c13601fa58cdfb19f21af0250d247c1dbe1470
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-31 01:38:40 +0300

    feat: provided access to selected value in file browser through the public part api

--- src/temt/file_browser.hpp
+++ src/temt/file_browser.hpp
@@ -5,4 +5,4 @@
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/component_base.hpp"
 
-ftxui::Component FileBrowser(std::string_view path);
+ftxui::Component FileBrowser(std::string_view path, int& a);
