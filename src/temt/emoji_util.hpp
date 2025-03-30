38ac5eb5453d85a1542010f3857423d0f39a1c18
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-30 22:30:12 +0300

    feat: created emoji-definer for directory entry

---
+++ src/temt/emoji_util.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "FileManip.hpp"
+
+namespace temt {
+namespace emoji {
+std::string emojiedFileName(FileManip::FileInfo file) {
+    switch (file.type) {
+        case FileManip::FileType::Directory:
+            return "📁" + file.path;
+        case FileManip::FileType::Character:
+            return "📝" + file.path;
+        default:
+            return "❓" + file.path;
+    }
+}
+}  // namespace emoji
+}  // namespace temt
