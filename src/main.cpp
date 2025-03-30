1e98bbc1825ab80cceacdf73f9a4876bc56617e3
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-03-30 22:32:32 +0300

    chore: updated main to run filebrowser in termnial for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,26 @@
 #include "FileManip.hpp"
 #include "CharProcessing.hpp"
 #include "DiskInfo.hpp"
+#include "file_browser.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/component.hpp"
+#include <filesystem>
 
 int main() {
     temt::Log::createFileLogger("logs/logs.txt");
+
+    auto screen = ftxui::ScreenInteractive::Fullscreen();
+    
+
+    auto fb = ftxui::Container::Horizontal({
+        FileBrowser(std::filesystem::current_path().c_str()),
+        ftxui::Renderer([&](){
+            return ftxui::text("hello");
+        })
+    });
+    
+    screen.Loop(fb);
+
+
     return 0;
 }
