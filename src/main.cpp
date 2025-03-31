215b04a5f88dec7baf7e3f634ce5e2c2a08ab6ff
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-31 20:36:59 +0300

    fix: broken exit now works the right way

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,20 @@
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
 #include "main_app.hpp"
+#include <cstdlib>
+
+auto screen = ftxui::ScreenInteractive::Fullscreen();
 
 void terminateApp(){
     auto fileLogger = spdlog::get("file_logger");
     fileLogger->flush();
+    screen.Exit();
 }
+
 
 
 int main() {
     temt::Log::createFileLogger("logs/logs.txt");
-
-    auto screen = ftxui::ScreenInteractive::Fullscreen();
-    
 
     screen.Loop(MainApp(terminateApp));
 
