923ed0378601013d11f9eff7baa99dd7ee880bfc
kotobazza <lumenos.kotobazza@gmail.com>
2025-03-31 18:42:39 +0300

    chore: playing with graphical alignment of components

--- src/temt/main_app.cpp
+++ src/temt/main_app.cpp
@@ -9,28 +9,35 @@ using namespace ftxui;
 class Impl : public ComponentBase {
    public:
     Impl() {
-        fileBrowser_ = FileBrowser(std::filesystem::current_path().c_str(), selectedFileBrowser_);
-        upperPanel_ =
-            Container::Horizontal({Button(" < ", [&]() { hiddenFileBrowserPanel_ = !hiddenFileBrowserPanel_; }),
-                                   Button("Menu", []() {}), Renderer([]() { return filler(); }),
-                                   Button("Help", []() {}), Button(" ✖ ", []() {})}) |
-            border;
+        exec_path = std::filesystem::current_path().c_str();
+        fileBrowser_ = Hideable({FileBrowser(exec_path, selectedFileBrowser_)},
+                                hiddenFileBrowserPanel_);
+        upperPanel_ = Container::Horizontal(
+            {Button(" < ", [&]() { hiddenFileBrowserPanel_ = !hiddenFileBrowserPanel_; }), Button("Menu", []() {}),
+             Button("Help", []() {}), Renderer([&](){return hbox({text("#>")|color(ftxui::Color::Yellow1), text(exec_path)});})|vcenter|bold, Renderer([]() { return filler(); }), Button(" ✖ ", []() {})});
 
-        mainPanel_ = Container::Vertical({Renderer([]() { return text("HelloWrold"); })});
+        logPanel_ = Renderer([]() { return text("Logs:"); }) | border;
+
+        mainPanel_ = Renderer([]() { return vbox({text("HelloWrold"), filler()}); }) | border;
 
         Add(Container::Vertical(
-            {upperPanel_, Container::Horizontal({Hideable({fileBrowser_}, hiddenFileBrowserPanel_), mainPanel_})}));
+            {upperPanel_,
+             Container::Horizontal({fileBrowser_, Container::Vertical({mainPanel_ | flex, logPanel_}) | flex_grow}) |
+                 yflex}));
     }
 
    private:
     ftxui::Component fileBrowser_;
     ftxui::Component upperPanel_;
     ftxui::Component mainPanel_;
+    ftxui::Component logPanel_;
+
+    std::string exec_path;
 
     int selectedFileBrowser_;
-    bool hiddenFileBrowserPanel_;
+    bool hiddenFileBrowserPanel_ = true;
 };
 
 Component MainApp() {
-    return Make<Impl>();
+    return Make<Impl>()|flex;
 }
