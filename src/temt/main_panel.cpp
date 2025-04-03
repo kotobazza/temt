92e2fb02dca12645a91f06bf202ff23bea43d072
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-04-03 19:26:37 +0300

    feat: main_panel is able to hot reset childs

--- src/temt/main_panel.cpp
+++ src/temt/main_panel.cpp
@@ -1,20 +1,63 @@
 #include "main_panel.hpp"
 
+#include "ftxui/component/event.hpp"
+
 using namespace ftxui;
 
 class Impl : public ComponentBase {
    public:
-    Impl() {
-        mainContainer_ = Container::Vertical({
-            Renderer([](){return text("MainPanel");})
-        });
-        Add(mainContainer_);
+    Impl(ftxui::Components children) {
+        for (auto child : children) {
+            Add(child);
+        }
+    }
+
+    void SetChildren(ftxui::Components new_children) {
+        children_.clear();
+        for (auto& child : new_children) {
+            Add(child);
+        }
+    }
+
+    Element OnRender() {
+        Elements elements;
+        for (auto& child : children_) {
+            elements.push_back(child->Render());
+        }
+        return vbox(std::move(elements));
+    }
+
+    bool Focusable() const final {
+        if (children_.size() == 0)
+            return false;
+        return children_[0]->Focusable();
+    }
+
+    Component ActiveChild() final {
+        if (children_.size() == 0)
+            return nullptr;
+        return children_[0];
+    }
+
+    bool OnEvent(Event event) final {
+        for (auto& child : children_) {
+            if (child->OnEvent(event)) {
+                return true;
+            }
+        }
+        return false;
     }
 
    private:
     ftxui::Component mainContainer_;
 };
 
-ftxui::Component MainPanel() {
-    return Make<Impl>() | flex_grow| border;
+ftxui::Component MainPanel(ftxui::Components childs) {
+    return Make<Impl>(childs) | flex_grow | border;
 };
+
+void UpdateChildren(Component main_panel, ftxui::Components new_children) {
+    if (auto impl = dynamic_cast<Impl*>(main_panel.get())) {
+        impl->SetChildren(std::move(new_children));
+    }
+}
--- src/temt/main_panel.hpp
+++ src/temt/main_panel.hpp
@@ -3,4 +3,6 @@
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/component_base.hpp"
 
-ftxui::Component MainPanel();
+ftxui::Component MainPanel(ftxui::Components childs);
+
+void UpdateChildren(ftxui::Component main_panel, ftxui::Components new_children);
