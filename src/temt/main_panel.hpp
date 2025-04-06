abbce7f8d387a744879076723a20b1af3b20a795
kotobazza <lumenos.kotobazza@gmail.com>
2025-04-07 01:36:18 +0300

    chore: cleanup of unuseful files

--- src/temt/main_panel.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ftxui/component/component.hpp"
-#include "ftxui/component/component_base.hpp"
-
-ftxui::Component MainPanel(ftxui::Components childs);
-
-void UpdateChildren(ftxui::Component main_panel, ftxui::Components new_children);
-
